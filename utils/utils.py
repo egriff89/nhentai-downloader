0d9a2ff4ed94a071b6b802099b17a027572c1e76
egriff89 <eric.griffith42@gmail.com>
2021-05-03 01:14:07 -0500

    Add verify_tags() function

--- utils/utils.py
+++ utils/utils.py
@@ -31,3 +31,51 @@ def remove_dir(dir_name):
         print(e)
         sys.exit(1)
     os.rmdir(dir_name)
+
+
+def verify_tags(doujinshi):
+    """Verify tag category information
+    
+    :param doujinshi:  Reference to Doujinshi instance to verify
+    """
+    if doujinshi.verify_tag_category('parody'): 
+        print(f'Parodies: ', end=' ')
+        for parody in doujinshi.parody:
+            if parody is None: pass
+            else: print(f'"{parody}"', end=' ')
+
+    if doujinshi.verify_tag_category('characters'):
+        print(f'\nCharacters: ', end='')
+        for ch in doujinshi.characters: 
+            if ch is None: pass
+            else: print(f'"{ch}"', end=' ')
+
+    if doujinshi.verify_tag_category('tags'):
+        print(f'\nTags: ', end='')
+        for tag in doujinshi.tags: 
+            if tag is None: pass
+            else: print(f'"{tag}"', end=' ')
+
+    if doujinshi.verify_tag_category('artists'):
+        print(f'\nArtists: ', end='')
+        for artist in doujinshi.artists: 
+            if artist is None: print('N/A', end=' ')
+            else: print(f'"{artist}"', end=' ')
+
+    if doujinshi.verify_tag_category('groups'):
+        print(f'\nGroups: ', end='')
+        for group in doujinshi.groups: 
+            if group is None: print('N/A', end=' ')
+            else: print(f'"{group}"', end=' ')
+
+    if doujinshi.verify_tag_category('languages'):
+        print(f'\nLanguages: ', end='')
+        for lang in doujinshi.languages: 
+            if lang is None: pass
+            else: print(f'"{lang}"', end=' ')
+
+    if doujinshi.verify_tag_category('categories'):
+        print(f'\nCategories: ', end='')
+        for cat in doujinshi.categories: 
+            if cat is None: pass
+            else: print(f'"{cat}"', end=' ')
